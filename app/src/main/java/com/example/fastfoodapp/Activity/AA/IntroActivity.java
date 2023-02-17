f6beb47dd10764998d85601e7340c211274572f0
San Võ <Sanvo1120@gmail.com>
2023-02-17 12:00:28 +0700

    twoActivity

---
+++ app/src/main/java/com/example/fastfoodapp/Activity/AA/IntroActivity.java
@@ -0,0 +1,27 @@
+package com.example.fastfoodapp.Activity.AA;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.fastfoodapp.R;
+
+public class IntroActivity extends AppCompatActivity {
+private ConstraintLayout startbtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        startbtn=findViewById(R.id.startbtn);
+        startbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IntroActivity.this,MainActivity.class));
+            }
+        });
+    }
+}
